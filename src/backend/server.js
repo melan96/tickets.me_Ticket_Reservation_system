908b738789863cea9764ce2deaff9050d80af22b
Melan Rashitha <it17006712@my.sliit.lk>
2019-05-01 10:45:16 +0530

    db connection string added and connection establishment

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,3 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(
+    "mongodb+srv://melan96:melan96@mongo-todo-yebxu.mongodb.net/test?retryWrites=true"
+  )
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(4001, () => {
+  console.log("listen to server ...");
+});
