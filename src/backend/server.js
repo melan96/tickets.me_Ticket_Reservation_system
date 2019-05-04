4043aa02189986fac947110c56d9717eeb66d78b
Melan Rashitha <it17006712@my.sliit.lk>
2019-05-04 07:59:34 +0530

    schema js added to database files

--- src/backend/server.js
+++ src/backend/server.js
@@ -20,6 +20,22 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+let Request = express.Router();
+Request = require("../backend/schema");
+
+Request.route("/addRequest").post((req, res) => {
+  let request = new Request(req.body);
+  request
+    .save()
+    .then(request => {
+      req.status(200);
+      console.log("added Request instance record..");
+    })
+    .catch(err => {
+      console.log(err + "==> captured");
+    });
+});
+
 app.listen(4001, () => {
   console.log("listen to server ...");
 });
